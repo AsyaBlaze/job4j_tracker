ed4bfe83d2ca81753c72de45b31a229ef6517664
Nastya <anastasiaalek67@yandex.ru>
2022-07-03 13:38:21 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(){
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void Info() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 2, "Ошибка номер два. Синтаксис");
+        Error error3 = new Error(false, 0, "Ошибка ноль. Нет ошибки");
+        error1.Info();
+        error2.Info();
+        error3.Info();
+    }
+}
