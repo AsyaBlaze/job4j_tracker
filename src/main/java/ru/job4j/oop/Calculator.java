fede5c8e789164342c0ad15576e91f367f070bb5
Nastya <anastasiaalek67@yandex.ru>
2022-07-04 18:37:59 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,41 @@
+package ru.job4j.oop;
+
+import java.util.concurrent.Callable;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static int minus(int number) {
+        return number - x;
+    }
+
+    public int divide(int number) {
+        return number / x;
+    }
+
+    public int sumAllOperation(int number) {
+        return sum(number) + multiply(number) + minus(number) + divide(number);
+    }
+
+    public static void main(String[] args) {
+        int result = Calculator.sum(10);
+        Calculator calculator = new Calculator();
+        int rsl = calculator.multiply(5);
+        int rslMinus = minus(10);
+        Calculator divides = new Calculator();
+        int rslDivide = divides.divide(10);
+        Calculator sum = new Calculator();
+        int rslFinal = sum.sumAllOperation(10);
+        System.out.println(result + " - Сумма\n" + rsl + " - Умножение\n" + rslMinus + " - Вычитание\n" + rslDivide + " - Деление\n" + rslFinal + " - Сумма всех операций\n");
+    }
+
+}
