43b51ec6babe3926acda937228d1b7b2652c8a2a
Nastya <anastasiaalek67@yandex.ru>
2022-07-07 21:07:34 +0300

    Репозиторий под проект Tracker

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+public class PointTest {
+    @Test
+    public void WhenPoints333And666() {
+        Point a = new Point(3, 3, 3);
+        Point b = new Point(6, 6, 6);
+        double rsl = b.distance3d(a);
+        assertThat(rsl, closeTo(5.1, 0.1));
+    }
+
+    @Test
+    public void WhenPoints000And666() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(6, 6, 6);
+        double rsl = b.distance3d(a);
+        assertThat(rsl, closeTo(10.3, 0.1));
+    }
+}
