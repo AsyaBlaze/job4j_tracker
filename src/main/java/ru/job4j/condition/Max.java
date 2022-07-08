ce7950ef6576ddb8271abf63425d81d076448e70
Nastya <anastasiaalek67@yandex.ru>
2022-07-08 11:30:54 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,12 +6,11 @@ public class Max {
     }
 
     public static int max(int one, int two, int three) {
-        return max(one, two) > three ? max(one, two) : three;
+        return max(max(one, two), three);
     }
 
     public static int max(int one, int two, int three, int four) {
-        int maximum = max(one, two, three);
-        return maximum > four ? maximum : four;
+        return max(max(max(one, two), three), four);
     }
 
     public static void main(String[] args) {
