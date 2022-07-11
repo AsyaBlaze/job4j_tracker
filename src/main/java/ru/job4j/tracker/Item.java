42e0f2c4b40a8906ac1b235a2cd28cbd8ecef125
Nastya <anastasiaalek67@yandex.ru>
2022-07-11 21:59:19 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -15,8 +15,7 @@ public class Item {
         this.id = id;
         this.name = name;
     }
-
-    public static void main(String[] args) {
-        Item item = new Item();
+    public int getId() {
+        return id;
     }
 }
