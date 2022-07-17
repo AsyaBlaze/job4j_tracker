748ec5c725b64b9582cba52fde52555344f67e82
Nastya <anastasiaalek67@yandex.ru>
2022-07-17 17:55:39 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,19 +1,25 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private static LocalDateTime created;
 
     public Item() {
+        created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
+        created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        created = LocalDateTime.now();
     }
 
     public int getId() {
@@ -26,6 +32,10 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getDate() {
+        return created;
     }
 
     public void setName(String name) {
