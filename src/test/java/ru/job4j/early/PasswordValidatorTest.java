218a480ca6f81afeac65769230afbe3c58149953
Asya Blaze <anastasiaalek67@yandex.ru>
2022-12-08 23:35:11 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/test/java/ru/job4j/early/PasswordValidatorTest.java
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.early;
 
 import org.junit.jupiter.api.Test;
-
+/
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
