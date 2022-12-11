aa7aac1f1f2078df3ee7221f0cd4d0b185fa3820
Asya Blaze <anastasiaalek67@yandex.ru>
2022-12-11 19:02:21 +0300

    3. final [#334073]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int temp = (name == "A380") ? 4 : COUNT_ENGINE;
+        int temp = (name.equals("A380")) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + temp);
     }
 
