6abe30c53343e1e0069b47c3dca0b6b5c4ef40ee
Asya Blaze <anastasiaalek67@yandex.ru>
2023-03-26 11:44:19 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("kazDirtyHands@bochka.ket", "Kaz Brak");
         map.put("kazDirtyHands@bochka.ket", "Kaz Brekker");
+        map.put("ghost@bochka.ket", "Inej Deg");
+        map.put("ghost@bochka.ket", "Inej Gafa");
+        map.put("shootHeart@bochka.ket", "Jesper Fahi");
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
         }
