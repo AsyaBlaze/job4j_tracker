77a50d3db5ed725a8ced90842a429477caac5eea
AsyaBlaze <anastasiaalek67@gmail.com>
2023-12-07 21:33:17 +0300

    2. Local-Variable Type Inference [#70626 #465151]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,7 +2,7 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 import java.util.function.Predicate;
-//
+
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
 
