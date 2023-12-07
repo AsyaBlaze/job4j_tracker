424afad28e2c5c2743b6d41ef6e1766fb182e975
AsyaBlaze <anastasiaalek67@gmail.com>
2023-12-07 18:30:27 +0300

    9. Генерирование колоды карт. FlatMap [#504873 #464678]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+//
 
 public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
