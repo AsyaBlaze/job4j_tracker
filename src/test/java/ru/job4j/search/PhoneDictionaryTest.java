e523b74db9cd599c77d497e5ec8693ebb3e8ec46
AsyaBlaze <anastasiaalek67@gmail.com>
2023-12-07 21:32:59 +0300

    2. Local-Variable Type Inference [#70626 #465151]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+//
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
